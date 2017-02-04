cc3aeab967644b13241dfaaf8bbf466f87b5bbce
Mike Vasiliou <mvasiliou94@gmail.com>
2017-02-04 14:24:19 -0600

    Hide field in form

--- spotify_convert/forms.py
+++ spotify_convert/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    spotify_code = forms.CharField(type="hidden", id="spotify-code", name='spotify-code', value="This is the default spotify code")
-    avatar_url = forms.CharField(type="hidden", id="avatar-url", name='avatar-url',value="/static/default.png")
+    spotify_code = forms.CharField(widget=forms.HiddenInput, id="spotify-code", name='spotify-code', value="This is the default spotify code")
+    avatar_url = forms.CharField(widget=forms.HiddenInput, id="avatar-url", name='avatar-url',value="/static/default.png")
