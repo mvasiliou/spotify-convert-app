92ea2079fc6a84f54f02282edee54deb104f3409
Mike Vasiliou <mvasiliou94@gmail.com>
2017-02-04 14:18:13 -0600

    Convert to django form model

--- spotify_convert/forms.py
+++ spotify_convert/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    spotify_code = forms.CharField()
-    avatar_url = forms.CharField()
+    spotify_code = forms.CharField(type="hidden", id="spotify-code", name='spotify-code', value="This is the default spotify code")
+    avatar_url = forms.CharField(type="hidden", id="avatar-url", name='avatar-url',value="/static/default.png")
