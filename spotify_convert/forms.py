e59ba7d88fdf4b771ad68459d476143608bad94e
Mike Vasiliou <mvasiliou94@gmail.com>
2017-02-04 14:28:18 -0600

    Remove name from form model

--- spotify_convert/forms.py
+++ spotify_convert/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    spotify_code = forms.CharField(widget=forms.HiddenInput, id="spotify-code", name='spotify-code', initial="This is the default spotify code")
-    avatar_url = forms.CharField(widget=forms.HiddenInput, id="avatar-url", name='avatar-url',initial="/static/default.png")
+    spotify_code = forms.CharField(widget=forms.HiddenInput, id="spotify-code", initial="This is the default spotify code")
+    avatar_url = forms.CharField(widget=forms.HiddenInput, id="avatar-url", initial="/static/default.png")
