5ab7b8f6083aed456df10bc71a203308b6be956e
Mike Vasiliou <mvasiliou94@gmail.com>
2017-02-04 14:37:28 -0600

    Set field to visible for testing

--- spotify_convert/forms.py
+++ spotify_convert/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    spotify_code = forms.CharField(widget=forms.HiddenInput(attrs={'id': 'spotify-code'}), initial="This is the default spotify code")
-    avatar_url = forms.CharField(widget=forms.HiddenInput(attrs={'id': 'avatar-url'}), initial="/static/default.png")
+    spotify_code = forms.CharField(widget=forms.CharField(attrs={'id': 'spotify-code'}), initial="This is the default spotify code")
+    avatar_url = forms.CharField(widget=forms.CharField(attrs={'id': 'avatar-url'}), initial="/static/default.png")
