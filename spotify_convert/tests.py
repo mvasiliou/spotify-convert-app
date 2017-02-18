876e456c956fd0aeae65dc1db9f683373bfa9c9e
Mike Vasiliou <mvasiliou94@gmail.com>
2017-02-18 11:25:00 -0600

    Rewrote tests pt.2

--- spotify_convert/tests.py
+++ spotify_convert/tests.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
+from .models import UserProfile
 
 # Create your tests here.
 
 def test_refresh():
-    from .models import UserProfile
     from .helper import refresh_token
     profile = UserProfile.objects.get(email="mvasiliou94@gmail.com")
     refresh_token(profile)
 
+
 def test_library():
-    from .models import UserProfile
     from .tasks import go
     profile = UserProfile.objects.get(email="mvasiliou94@gmail.com")
     go('library.xml',profile)
